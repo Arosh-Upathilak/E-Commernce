26c492ed66b8e1166e2c06f6c13171373300161b
Adun Arosh Upathilak <aroshupathilak@gmail.com>
2025-06-16 21:48:32 +0530

    add the navbar

--- forntend/src/App.js
+++ forntend/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div>Hello world</div>
     </div>
   );
