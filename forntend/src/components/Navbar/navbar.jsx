b27e379f5e12398cbc339cb5ac10b1c7638daab3
Arosh-Upathilak <aroshupathilak.com>
2025-06-24 00:35:45 +0530

    add the cart item

--- forntend/src/components/Navbar/navbar.jsx
+++ forntend/src/components/Navbar/navbar.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './navbar.css';
 import {assets} from '../../assets/assets'
+import { StoreContext } from '../../contex/StoreContext';
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home");
 
-
+    const {getTocartTotal} =useContext(StoreContext)
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" className="logo" />
+        <Link to=''><img src={assets.logo} alt="" className="logo" /> </Link>
         <ul className="navbar-menu">
             <li className={menu==="home"?"active":""} onClick={()=>setMenu("home")}>home</li>
             <li className={menu==="menu"?"active":""} onClick={()=>setMenu("menu")}>menu</li>
@@ -19,11 +21,11 @@ const Navbar = () => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /> </Link>
+                <div className={getTocartTotal()===0?"":"dot"}></div>
                 
             </div>
-            <button>sign in</button>
+            <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
         
     </div>
--- forntend/src/contex/StoreContext.jsx
+++ forntend/src/contex/StoreContext.jsx
@@ -1,41 +1,52 @@
 /*This React context setup is used to make food_list accessible to all components in your app without passing it manually as props.*/
 
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
-const StoreContextProvider =(props) =>{
+const StoreContextProvider = (props) => {
 
-    const [cartItem,setCartItem]=useState({});
+    const [cartItem, setCartItem] = useState({});
 
-        const addToCart=(itemId)=>{
-            if(!cartItem[itemId]){
-                setCartItem((prev)=>({...prev,[itemId]:1}))
-            }
-            else{
-                setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-            }
+    const addToCart = (itemId) => {
+        if (!cartItem[itemId]) {
+            setCartItem((prev) => ({ ...prev, [itemId]: 1 }))
         }
-
-        const removeFromCart=(itemId)=>{
-            setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        else {
+            setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
-        useEffect(()=>{
-            console.log(cartItem)
-        },[cartItem])
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItem((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    const getTocartTotal = () => {
+        let totalAmount = 0;
+        for (const item in cartItem) {  //item:key value
+            if (cartItem[item] > 0) {
+                                        // this is a funcion
+                let iteminfo = food_list.find((product) => product._id === item)
+                totalAmount += iteminfo.price * cartItem[item]
+            }
+
+        }
+        return totalAmount
+    }
 
 
-    const contextValue ={
+    const contextValue = {
         food_list,
         cartItem,
         setCartItem,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTocartTotal
     }
 
 
-    return(
+    return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
