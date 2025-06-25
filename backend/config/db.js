9264b0f59cc863a6a9f846620d26b9b8643b0ba3
Arosh-Upathilak <aroshupathilak.com>
2025-06-25 23:29:21 +0530

    add the mongodb connection

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+  try {
+    const mongoUrl = process.env.MONGODB_URL;
+    if (!mongoUrl) throw new Error('MONGODB_URL not found in .env');
+
+    await mongoose.connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(()=>console.log("MongoDB connected"));
+
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
+};
